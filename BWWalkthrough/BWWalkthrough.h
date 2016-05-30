9616ecd481a72de561400a8c810099ba220d22a7
Shantanu R Desai <gokuman76@gmail.com>
2016-05-31 01:28:17 +0530

    Updates..

---
+++ BWWalkthrough/BWWalkthrough.h
@@ -0,0 +1,19 @@
+//
+//  BWWalkthrough.h
+//  BWWalkthrough
+//
+//  Created by Shantanu Desai on 31/05/16.
+//  Copyright © 2016 Shantanu Desai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BWWalkthrough.
+FOUNDATION_EXPORT double BWWalkthroughVersionNumber;
+
+//! Project version string for BWWalkthrough.
+FOUNDATION_EXPORT const unsigned char BWWalkthroughVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BWWalkthrough/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+#BWWalkThrough1
+
+A Library to support the original [BWWalkThrough](https://github.com/ariok/BWWalkthrough) by Yari D'areglia. 
+
+Please refer to the readme of [BWWalkThrough](https://github.com/ariok/BWWalkthrough) for usage. 
